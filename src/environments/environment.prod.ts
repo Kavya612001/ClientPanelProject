2116379154e6c8e96657abc6760160a8cdf06221
Kavya Sampath Kumar <ksampathkumar@zuora.com>
2023-03-05 00:54:13 +0530

    files changed as in lec

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCdvCCL8kPxKLq53x3y_5gdraU3NYhSihk",
+    authDomain: "clientpanelprod-b28f5.firebaseapp.com",
+    projectId: "clientpanelprod-b28f5",
+    storageBucket: "clientpanelprod-b28f5.appspot.com",
+    messagingSenderId: "661900267583",
+    appId: "1:661900267583:web:3227ca195cd3b7a14ba88a",
+    measurementId: "G-QNB4JFWR8N"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCdvCCL8kPxKLq53x3y_5gdraU3NYhSihk",
+    authDomain: "clientpanelprod-b28f5.firebaseapp.com",
+    projectId: "clientpanelprod-b28f5",
+    storageBucket: "clientpanelprod-b28f5.appspot.com",
+    messagingSenderId: "661900267583",
+    appId: "1:661900267583:web:3227ca195cd3b7a14ba88a",
+    measurementId: "G-QNB4JFWR8N"
+  }
+};
