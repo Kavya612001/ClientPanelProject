ea27feb0d8eedeaed6358bc6d9aa17f75ffce199
Kavya Sampath Kumar <ksampathkumar@zuora.com>
2023-03-05 19:05:39 +0530

    login fucntionality added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { ClientService } from './services/client.service';
 import { FormsModule } from '@angular/forms';
+import { AuthService } from './services/auth.service';
 
 
 @NgModule({
@@ -49,7 +50,10 @@ import { FormsModule } from '@angular/forms';
     FormsModule,
     FlashMessagesModule.forRoot()
   ],
-  providers: [ClientService],
+  providers: [
+    ClientService,
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
